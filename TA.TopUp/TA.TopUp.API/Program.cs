201532a968bb1e9f55d2b1024494b46cc351f000
Shivvvvvvvvv <109672286+Shivvvvvvvvv@users.noreply.github.com>
2024-04-04 15:30:42 +0400

    Added Global Middleware and HTTP Service wallet balance.  Created Error Response model

--- TA.TopUp/TA.TopUp.API/Program.cs
+++ TA.TopUp/TA.TopUp.API/Program.cs
@@ -1,10 +1,12 @@
 using Serilog;
 using Serilog.Formatting.Compact;
 using Serilog.ThrowContext;
+using TA.TopUp.API.Middleware;
 using TA.TopUp.ApplicationService;
 using TA.TopUp.Core.Interfaces.Repositories;
 using TA.TopUp.Core.Interfaces.Services;
 using TA.TopUp.Infrastructure.DataAccessAbstractions;
+using TA.TopUp.Infrastructure.ExternalServices;
 using TA.TopUp.Infrastructure.Repositories;
 using TA.TopUp.Shared.Options;
 
@@ -26,12 +28,18 @@ try
 
     builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
     builder.Services.AddScoped<IBeneficiaryService,BeneficiaryService>();
+    //builder.Services.AddScoped<WalletService>();
+    builder.Services.AddHttpClient<WalletService>();
     builder.Services.AddScoped<ITopUPService, TopUpService>();
     builder.Services.AddScoped<IBeneficiaryRepository, BeneficiaryRepository>();
     builder.Services.AddScoped<IUserTransactionsRepository, UserTransactionsRepository>();
     builder.Services.AddScoped<IUserWalletBalancesRepository, UserWalletBalancesRepository>();
     builder.Services.AddScoped<ITopUpOptionsRepository, TopUpOptionsRepository>();
     builder.Services.Configure<BeneficiariesTopUpValidation>(builder.Configuration.GetSection(nameof(BeneficiariesTopUpValidation)));
+    builder.Services.Configure<WalletServiceConfig>(builder.Configuration.GetSection(nameof(WalletServiceConfig)));
+
+    // Global error handler
+    
 
     builder.Services.AddControllers();
     builder.Services.AddSwaggerGen();
@@ -39,7 +47,7 @@ try
     var app = builder.Build();
     app.UseHttpsRedirection();
     app.MapControllers();
-
+    app.UseMiddleware<ExceptionHandlingMiddleware>();
     if (app.Environment.IsDevelopment())
     {
         app.UseSwagger();
--- TA.TopUp/src/TA.TopUp.ApplicationService/TopUpService.cs
+++ TA.TopUp/src/TA.TopUp.ApplicationService/TopUpService.cs
@@ -1,13 +1,9 @@
 ﻿using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using TA.TopUp.Core.Entities;
 using TA.TopUp.Core.Interfaces.Services;
 using TA.TopUp.Infrastructure.DataAccessAbstractions;
+using TA.TopUp.Infrastructure.ExternalServices;
 using TA.TopUp.Shared.DTOs.Request;
 using TA.TopUp.Shared.DTOs.Response;
 using TA.TopUp.Shared.Options;
@@ -19,11 +15,37 @@ namespace TA.TopUp.ApplicationService
         private readonly ILogger<TopUpService> _logger;
         private readonly IUnitOfWork _unitOfWork;
         private readonly BeneficiariesTopUpValidation _beneficiariesTopUpValidation;
-        public TopUpService(ILogger<TopUpService> logger, IUnitOfWork unitOfWork, IOptionsMonitor<BeneficiariesTopUpValidation> beneficiariesTopUpValidation) 
+        private readonly WalletService _walletService;
+        public TopUpService(WalletService walletService, ILogger<TopUpService> logger, IUnitOfWork unitOfWork, IOptionsMonitor<BeneficiariesTopUpValidation> beneficiariesTopUpValidation) 
         {
+            _walletService = walletService;
             _logger = logger;
             _unitOfWork = unitOfWork;
             _beneficiariesTopUpValidation = beneficiariesTopUpValidation.CurrentValue;
+        }
+
+        private static UserTransaction InsertUserTransaction(int userId, TopUpBeneficiaryRequest request)
+        {
+            UserTransaction userTransaction = new UserTransaction();
+            userTransaction.UserId = userId;
+            userTransaction.BeneficiaryId = request.BeneficiaryId;
+            userTransaction.Amount = request.Amount;
+            userTransaction.TransactionType = "Debit";
+            userTransaction.CurrencyId = 1;
+            userTransaction.CreatedAt = DateTime.UtcNow;
+            userTransaction.CreatedBy = userId.ToString();
+            return userTransaction;
+        }
+        public async Task<IEnumerable<TopUpOptionResponse>> GetTopUpOptions(int userId)
+        {
+            var topUpOptions = (await _unitOfWork.TopUpOptionsRepository.GetAsync(x => x.Amount != null))
+                               .Select(s => new TopUpOptionResponse
+                               {
+                                   UId = s.Uid,
+                                   Amount = s.Amount,
+                                   Currency = s.Currency?.Currency1 ?? "AED"
+                               });
+            return topUpOptions;
         }
         public async Task<TopUpResponse> TopUpBeneficiary(int userId, TopUpBeneficiaryRequest request)
         {
@@ -68,35 +90,43 @@ namespace TA.TopUp.ApplicationService
                         //Refract below methos
                         if(verifyTopUpOption != null)
                         {
-                            //user Balance
-                            int topUpBalance = 10;
-                            long walletId = 1;
 
-                            //balance
+
+                            var walletBalance = await _walletService.GetWalletBalance(userId);
+                            //user Balance
+                            decimal? topUpBalance = walletBalance.Amount;
+                            long walletId = walletBalance.WalletId;
+
+                            //TopUpCharge
+
 
                             //Debit from wallet balance
                             UserWalletBalance userWalletBalance = new UserWalletBalance();
                             userWalletBalance.Uid = walletId;
                             userWalletBalance.UserId = userId;
-                            userWalletBalance.Balance = topUpBalance - request.Amount;
+                            userWalletBalance.Balance = topUpBalance - (request.Amount + _beneficiariesTopUpValidation.TopUpCharge);
                             userWalletBalance.CurrencyId = 1;
-                            
+
                             userWalletBalance.LastUpdatedAt = DateTime.UtcNow;
                             userWalletBalance.LastUpdatedBy = userId.ToString();
 
                             _unitOfWork.UserWalletBalancesRepository.Update(userWalletBalance);
 
                             //Insert into transaction table
-                            UserTransaction userTransaction = new UserTransaction();
+                            UserTransaction userTransaction = InsertUserTransaction(userId, request);
+                            _unitOfWork.UserTransactionsRepository.Insert(userTransaction);
+
+                            //Insert into transaction table
+                            UserTransaction userTransactionCharge = new UserTransaction();
                             userTransaction.UserId = userId;
                             userTransaction.BeneficiaryId = request.BeneficiaryId;
-                            userTransaction.Amount = request.Amount;
+                            userTransaction.Amount = _beneficiariesTopUpValidation.TopUpCharge;
                             userTransaction.TransactionType = "Debit";
                             userTransaction.CurrencyId = 1;
                             userTransaction.CreatedAt = DateTime.UtcNow;
                             userTransaction.CreatedBy = userId.ToString();
 
-                            _unitOfWork.UserTransactionsRepository.Insert(userTransaction);
+                            _unitOfWork.UserTransactionsRepository.Insert(userTransactionCharge);
 
                             topUpResponse.IsSuccess = await _unitOfWork.SaveEntitiesAsync();
                             topUpResponse.Message = "Topup succeed";
@@ -136,16 +166,6 @@ namespace TA.TopUp.ApplicationService
             return topUpResponse;
         }
 
-        public async Task<IEnumerable<TopUpOptionResponse>> GetTopUpOptions(int userId)
-        {
-            var topUpOptions = (await _unitOfWork.TopUpOptionsRepository.GetAsync(x=>x.Amount != null))
-                               .Select(s => new TopUpOptionResponse
-                               {
-                                   UId = s.Uid,
-                                   Amount = s.Amount,
-                                   Currency = s.Currency?.Currency1??"AED"
-                               });
-            return topUpOptions;
-        }
+
     }
 }
